de116e9e7df332b58267f26562364d886a7d3ec1
Jackson Silva <jackson.silva.who@gmail.com>
2019-03-29 14:37:53 -0300

    Criação do layout da página principal (finalizado)

--- app/src/main/java/com/jackson/jokenpo/MainActivity.java
+++ app/src/main/java/com/jackson/jokenpo/MainActivity.java
@@ -2,6 +2,7 @@ package com.jackson.jokenpo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        );
+
     }
 }
