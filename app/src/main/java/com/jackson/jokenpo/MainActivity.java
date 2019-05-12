619d022f4e48140fcb52f5891a243fdb563d10aa
Jackson Silva <jackson.silva.who@gmail.com>
2019-05-12 16:35:38 -0300

    Adicionada programação para contagem de vitórias, derrotas e empates.

--- app/src/main/java/com/jackson/jokenpo/MainActivity.java
+++ app/src/main/java/com/jackson/jokenpo/MainActivity.java
@@ -13,6 +13,10 @@ public class MainActivity extends AppCompatActivity {
     ImageView escolhaMaqView;
     ImageView escolhaUsuView;
     TextView resultado;
+    TextView txtVitorias;
+    TextView txtDerrotas;
+    TextView txtEmpates;
+    int vitorias = 0, derrotas = 0, empates = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +26,9 @@ public class MainActivity extends AppCompatActivity {
         escolhaMaqView = (ImageView) findViewById(R.id.escolhaMaqView);
         escolhaUsuView = (ImageView) findViewById(R.id.escolhaUsuView);
         resultado = (TextView) findViewById(R.id.resultado);
+        txtVitorias = (TextView) findViewById(R.id.txtVitorias);
+        txtDerrotas = (TextView) findViewById(R.id.txtDerrotas);
+        txtEmpates = (TextView) findViewById(R.id.txtEmpates);
     }
 
     public void pedra(View view){this.opcaoSelecionada("pedra");}
@@ -34,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
         else {escolhaMaqView.setImageResource(R.drawable.tesoura);}
     }
 
-    public void mudarImagemEscolaUser(String escolhaUsuario){
+    public void mudarImagemEscolhaUser(String escolhaUsuario){
         if(escolhaUsuario.equals("pedra")){escolhaUsuView.setImageResource(R.drawable.pedra);}
         else if(escolhaUsuario.equals("papel")){escolhaUsuView.setImageResource(R.drawable.papel);}
         else{escolhaUsuView.setImageResource(R.drawable.tesoura);}
@@ -47,29 +54,35 @@ public class MainActivity extends AppCompatActivity {
         int indice = new Random().nextInt(opcoes.length);
         String escolhaApp = opcoes[indice];
 
+
         //Usuário Ganha
         if(escolhaUsuario=="pedra" && escolhaApp=="tesoura"
                 || escolhaUsuario=="papel" && escolhaApp=="pedra"
                 || escolhaUsuario=="tesoura" && escolhaApp=="papel"){
             mudarImagemEscolhaApp(escolhaApp);
-            mudarImagemEscolaUser(escolhaUsuario);
+            mudarImagemEscolhaUser(escolhaUsuario);
             resultado.setText("Você venceu!");
-
+            vitorias++;
+            txtVitorias.setText(String.valueOf(vitorias));
 
             //App Ganha
         }else if (escolhaApp=="pedra" && escolhaUsuario=="tesoura"
                 || escolhaApp=="papel" && escolhaUsuario=="pedra"
                 || escolhaApp=="tesoura" && escolhaUsuario=="papel"){
             mudarImagemEscolhaApp(escolhaApp);
-            mudarImagemEscolaUser(escolhaUsuario);
+            mudarImagemEscolhaUser(escolhaUsuario);
             resultado.setText("Você perdeu!");
+            derrotas++;
+            txtDerrotas.setText(String.valueOf(derrotas));
+
             //Empate
         }else{
             mudarImagemEscolhaApp(escolhaApp);
-            mudarImagemEscolaUser(escolhaUsuario);
+            mudarImagemEscolhaUser(escolhaUsuario);
             resultado.setText("Empate!");
+            empates++;
+            txtEmpates.setText(String.valueOf(empates));
         }
-
 
     }
 }
